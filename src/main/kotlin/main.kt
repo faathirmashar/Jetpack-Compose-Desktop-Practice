88eee966c0afccc8607343b956517ff4ec19c9a2
AudiFaathirmansyahMashar <audifaathirmansyahmashar@gmail.com>
2021-01-17 07:32:45 +0800

    Appbar + Drawer

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,6 +20,7 @@ import androidx.compose.ui.window.Dialog
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import javax.swing.text.TableView
 
 private val Yellow200 = Color(0xffffeb46)
 private val Blue200 = Color(0xff91a4fc)
@@ -66,42 +70,52 @@ fun mainWindow() {
             }
         }
 
+        val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
         MaterialTheme(colors = LightColors) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+            Scaffold(
+                topBar = {TopAppBar(title = { Text("Latihan")}, navigationIcon = {Icon(Icons.Default.Menu, modifier = Modifier.clickable(onClick = {
+                    scaffoldState.drawerState.open()
+                }))})},
+                drawerContent = {},
+                scaffoldState = scaffoldState
             ) {
-                Text(
-                    text = "Login",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(Modifier.preferredHeight(height = 16.dp))
-                OutlinedTextField(
-                    value = lat ?: "",
-                    onValueChange = { lat = it },
-                    label = { Text(text = "Username") },
-                    modifier = Modifier.background(Color.White)
-                )
-                Spacer(Modifier.preferredHeight(height = 16.dp))
-                OutlinedTextField(
-                    value = lat ?: "",
-                    onValueChange = { lat = it },
-                    label = { Text(text = "Password") },
-                )
-                Spacer(Modifier.preferredHeight(height = 16.dp))
-                Row {
-                    Button(
-                        onClick = {},
-                    ) {
-                        Text("Register")
-                    }
-                    Spacer(Modifier.preferredWidth(width = 32.dp))
-                    Button(
-                        onClick = { dialogState.value = true }
-                    ) {
-                        Text("Login")
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Login",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(Modifier.preferredHeight(height = 16.dp))
+                    OutlinedTextField(
+                        value = lat ?: "",
+                        onValueChange = { lat = it },
+                        label = { Text(text = "Username") },
+                        modifier = Modifier.background(Color.White)
+                    )
+                    Spacer(Modifier.preferredHeight(height = 16.dp))
+                    OutlinedTextField(
+                        value = lat ?: "",
+                        onValueChange = { lat = it },
+                        label = { Text(text = "Password") },
+                    )
+                    Spacer(Modifier.preferredHeight(height = 16.dp))
+                    Row {
+                        Button(
+                            onClick = {},
+                        ) {
+                            Text("Register")
+                        }
+                        Spacer(Modifier.preferredWidth(width = 32.dp))
+                        Button(
+                            onClick = { dialogState.value = true }
+                        ) {
+                            Text("Login")
+                        }
                     }
                 }
             }
